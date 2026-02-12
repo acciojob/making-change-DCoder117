17982fdb54870a5e81f04456dc6ac38606bc62c5
DCoder117 <dhirajopjadhav17@gmail.com>
2026-02-12 16:47:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const makeChange = (c) => {
   // your name here
+	let obj = {
+		let quarter = Math.floor(c/25);
+		let dime = Math.floor(quarter/10);
+		let nickel = Math.floor(dime/5);
+		let penny = math.floor(nickel/1);
+	}
+	return obj;
 };
 
 // Do not the change the code below
